cb3912fea91ac78a0ce4e6b3fbf1523cb234146a
ravenera0317 <r6090408@gmail.com>
2021-10-04 15:36:59 +0800

    get admin categories api

--- src/views/AdminCategories.vue
+++ src/views/AdminCategories.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="container py-5">
-    <!-- 1. 使用先前寫好的 AdminNav -->
     <AdminNav />
 
     <form class="my-4">
@@ -62,61 +61,41 @@
 
 <script>
 import AdminNav from '@/components/AdminNav'
+import adminAPI from './../apis/admin'
 import { v4 as uuidv4 } from 'uuid'
-
-//  2. 定義暫時使用的資料
-const dummyData = {
-  categories: [
-    {
-      id: 1,
-      name: '中式料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z',
-    },
-    {
-      id: 2,
-      name: '日本料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z',
-    },
-    {
-      id: 3,
-      name: '義大利料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z',
-    },
-    {
-      id: 4,
-      name: '墨西哥料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z',
-    },
-  ],
-}
+import { Toast } from './../utils/helpers'
 
 export default {
   components: {
     AdminNav,
   },
-  // 3. 定義 Vue 中使用的 data 資料
   data() {
     return {
-      newCategiryName: '',
+      newCategoryName: '',
       categories: [],
+      isLoading: true,
     }
   },
-  // 5. 調用 `fetchCategories` 方法
   created() {
     this.fetchCategories()
   },
   methods: {
-    // 4. 定義 `fetchCategories` 方法，把 `dummyData` 帶入 Vue 物件
-    fetchCategories() {
-      this.categories = dummyData.categories.map(category => ({
-        ...category,
-        isEditing: false,
-        nameCached: '',
-      }))
+    async fetchCategories() {
+      try {
+        const { data } = await adminAPI.categories.get()
+        this.categories = data.categories.map(category => ({
+          ...category,
+          isEditing: false,
+          nameCached: '',
+        }))
+        this.isLoading = false
+      } catch (error) {
+        this.isLoading = false
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳類別，請稍後再試',
+        })
+      }
     },
     createCategory(name) {
       // TODO: 透過 API 告知伺服器欲新增的餐廳類別...
